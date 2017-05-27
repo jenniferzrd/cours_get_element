e5858bd996b48835c1b241bc9a1d70ed219b2aab
jenniferzrd <jennifer.zarid@gmail.com>
2017-05-27 19:27:20 +0200

    first commit

--- utility.js
+++ utility.js
@@ -1,18 +1,21 @@
 var log, byId, select, selectAll;
 
 log = function log(val) {
-  "use strict";
+   "use strict";
+   window.console.log(val);
 };
 
-log = function byId(id) {
-  "use strict";
-  return document.getElementById(id);
+byId = function byId(id) {
+   "use strict";
+   return document.getElementById(id);
 };
-log = function select(sCss) {
-  "use strict";
-    return document.querySelector(sCss);
+
+select = function select(sCSS) {
+   "use strict";
+   return document.querySelector(sCSS);
 };
-log = function selectAll(sCss) {
-  "use strict";
-  return document.querySelectorAll(sCss);
+
+selectAll = function selectAll(sCSS) {
+   "use strict";
+   return document.querySelectorAll(sCSS);
 };
